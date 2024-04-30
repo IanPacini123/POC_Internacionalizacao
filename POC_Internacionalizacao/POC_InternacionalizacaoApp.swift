6649c99c4b52c3bb6ece654f30ba8f9b83c17b52
IanPacini <ianpacini@hotmail.com>
2024-04-30 16:57:34 -0300

    Initial Commit

---
+++ POC_Internacionalizacao/POC_InternacionalizacaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_InternacionalizacaoApp.swift
+//  POC_Internacionalizacao
+//
+//  Created by Ian Pacini on 30/04/24.
+//
+
+import SwiftUI
+
+@main
+struct POC_InternacionalizacaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
